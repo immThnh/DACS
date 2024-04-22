c9ae4affa0b0780ed6462d4e98d64af23ac9655b
sheam <sheam@ELITE>
2024-04-22 23:54:33 +0700

    hoan thanh giao dien hoc

--- FE/dacs/src/router/index.js
+++ FE/dacs/src/router/index.js
@@ -1,14 +1,14 @@
 import SignUp from "../pages/signUp";
 import Login from "../pages/login"
 import LandingPage from "../pages/landingpage/LandingPageComponent"
-import Addcourse from "../pages/addCourse"
 import details from "../pages/CourseDetail"
+import Course from "../pages/learningPage"
 const publicRoutes = [
     { path: "/home", component: LandingPage },
     { path: "/sign-up", component: SignUp },
     {path: "/login", component: Login},
-    {path: "/addcourse", component: Addcourse},
-    {path: "/details", component: details}
+    {path: "/details", component: details},
+    {path: "/course", component: Course}
 
 ];
 
